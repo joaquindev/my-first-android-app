a37b7642f0d1707abd1fbeee98221d67588f49f6
Joaquin <joaquin@isla.io>
2014-02-18 11:05:56 +0100

    Add a ListView
    Add a ListView and its associate ArrayAdapter and ArrayList where we are
    going to store the data.
    
    Contact: joaquin@isla.io

--- OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
+++ OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
@@ -10,15 +10,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     TextView mainTextView;
     Button mainButton;
     EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         //3. Access the EditText defined in layout XML
         mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        //4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+        //4.1 Create the ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this,
+                        android.R.layout.simple_list_item_1,
+                        mNameList);
+        //4.2 Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
 
         /*if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
@@ -71,6 +87,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View view) {
         mainTextView.setText(mainEditText.getText().toString()
                 + " is learning Android development from a tutorial!");
+
+        //4.4 Also add that value to the list shown in the ListView
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
     }
 
     /**
